de77e1e9da7afb3fc55214a75d2dc2dfcec38054
nuri <nooti777712@gmail.com>
2019-03-27 17:32:49 -0700

    add ShortenedUrl:random_code

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,3 +1,14 @@
 class ShortenedUrl < ApplicationRecord
   validates :short_url, presence: true, uniqueness: true
+
+  #   Associations
+
+  #   ==================================================
+  def self.random_code
+    random_code = SecureRandom.urlsafe_base64(16)
+    until !ShortenedUrl.exists?(short_url: random_code)
+      random_code = SecureRandom.urlsafe_base64(16)
+    end
+    random_code
+  end
 end
